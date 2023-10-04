6e77c029256e2f5497348b1eb54d2685f98cab7d
ngarukaalx <hiramkabuagi@gmail.com>
2023-10-04 18:15:27 +0000

    last

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #  match a 10 digit phone number
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flag:(.*?)\]\/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flag:(.*?)\]\//).join(",")
