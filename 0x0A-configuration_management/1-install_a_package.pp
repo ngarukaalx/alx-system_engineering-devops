682535f570b4c9940f3fb55075b6c6b8a3707768
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-24 18:37:25 +0000

    flsk

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 #  install flask from pip3.
-package {'python3-pip':
-ensure => 'installed'
-}
-#
-package {'flask':
-ensure   => '2.1.0',
+package {'Werkzeug':
+ensure   => '2.2.2',
 provider => 'pip3',
-require => 'package[python3-pip]'
 }
