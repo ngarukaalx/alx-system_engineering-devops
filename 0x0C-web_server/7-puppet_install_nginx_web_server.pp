fd9111d208aaba9b198c6f0d6c9e897a1cbc66ed
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-29 04:30:13 +0000

    puppet making things easy

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Installs nginx and configures
+
+package {'nginx':
+  ensure => 'installed',
+}
+
+# Helllo world index.html
+file {'/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+}
+
+# Redirect 301, listent port 80
+file_line {'nginx_redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://madukaonline.co.ke/ permanent;',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => package['nginx']
+}
