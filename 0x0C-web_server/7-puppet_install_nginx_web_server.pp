17b87c94bdea09b92db6782f45be77627ef97635
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-29 06:13:08 +0000

    nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,21 +4,30 @@ package {'nginx':
   ensure => installed,
 }
 
+exec {'update':
+  command  => 'apt-get update',
+  provider => shell,
+  notify   => Exec['install'],
+}
+
+exec {'install':
+  command  => 'apt-get -y install nginx',
+  provider => shell,
+  notify   => Exec['configure_nginx'],
+}
+
 # Helllo world index.html
-file {'/var/www/html/index.html':
-  ensure  => 'present',
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
 # Redirect 301, listent port 80
-file_line {'nginx_redirect':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://madukaonline.co.ke/ permanent;',
+exec { 'configure_nginx':
+  command  => '/usr/bin/sedsed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/madukaonline.co.ke\/ permanent/" /etc/nginx/sites-enabled/default',
+  notify   => Exec['restart_nginx'],
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => package['nginx']
+exec { 'restart_nginx':
+  command  => 'service nginx restart',
+  provider => shell,
 }
