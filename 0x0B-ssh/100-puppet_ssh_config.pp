f3260ab2026b8285f617bcaca729be3b6ac845f9
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-29 03:16:35 +0000

    puppet fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,9 @@
 #  using Puppet to make changes to our configuration file
-file {'/home/vagrant/.ssh/config':
+file {'/etc/ssh/ssh_config':
 ensure  => present,
-content => template('/home/vagrant/alx-system_engineering-devops/0x0B-ssh/ssh_config.erb'),
+content => "
+Host *
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
 }
