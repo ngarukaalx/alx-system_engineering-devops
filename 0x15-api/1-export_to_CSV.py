5cd59e88f81821997894e3a0029409d744ef1059
ngarukaalx <hiramkabuagi@gmail.com>
2024-01-03 10:20:46 +0000

    csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Returns a user information about his/her TODO list progress"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    """Only execute when run directly"""
+    employee_id = sys.argv[1]
+    csv_file = "USER_ID.csv"
+
+    if employee_id.isdigit():
+        base_url = "https://jsonplaceholder.typicode.com"
+        user_url = f"{base_url}/users/{employee_id}"
+        todo_url = f"{user_url}/todos"
+
+        # fetch user info
+        user_info = requests.get(user_url)
+        user_data = user_info.json()
+
+        # todo list for the user
+        todos_info = requests.get(todo_url)
+        todos_data = todos_info.json()
+
+        # create CSV file
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            for todo in todos_data:
+                writer.writerow(
+                        [employee_id, user_data['name'],
+                            todo['completed'], todo['title']])
