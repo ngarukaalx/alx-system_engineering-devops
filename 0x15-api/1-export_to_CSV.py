3c31b2e0998ef1b05bc2592f1b8190efcf507c8c
ngarukaalx <hiramkabuagi@gmail.com>
2024-01-03 11:04:42 +0000

    csv fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -22,6 +22,13 @@ if __name__ == "__main__":
         # todo list for the user
         todos_info = requests.get(todo_url)
         todos_data = todos_info.json()
+        # calculate completed tasks
+        complete_tasks = [todo for todo in todos_data if todo["completed"]]
+        total_tasks = len(todos_data)
+        print(f"Employee {user_data['name']} is done with tasks"
+              f"({len(complete_tasks)}/{total_tasks}):")
+        for task in complete_tasks:
+            print(f"\t {task['title']}")
 
         # create CSV file
         with open(csv_file, 'w', newline='') as file:
