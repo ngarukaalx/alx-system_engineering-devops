aff1079615db2661a1561906aa732819002e5bcc
ngarukaalx <hiramkabuagi@gmail.com>
2024-01-03 08:39:28 +0000

    api fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,19 +11,19 @@ if __name__ == "__main__":
     if employee_id.isdigit():
         base_url = "https://jsonplaceholder.typicode.com"
         user_url = f"{base_url}/users/{employee_id}"
-        todo_url = f"{base_url}/todos"
+        todo_url = f"{user_url}/todos"
 
         # fetch user info
         user_info = requests.get(user_url)
         user_data = user_info.json()
 
         # todo list for the user
-        todos_info = requests.get(todo_url, params={"user.id": employee_id})
+        todos_info = requests.get(todo_url)
         todos_data = todos_info.json()
         # calculate completed tasks
         complete_tasks = [todo for todo in todos_data if todo["completed"]]
         total_tasks = len(todos_data)
         print(f"Employee {user_data['name']} is done with tasks"
-                f"({len(complete_tasks)}/{total_tasks}):")
+              f"({len(complete_tasks)}/{total_tasks}):")
         for task in complete_tasks:
             print(f"\t {task['title']}")
