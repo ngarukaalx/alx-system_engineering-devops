ecb737b91d395c38e3d50ddd935bc1cdb323b00f
ngarukaalx <hiramkabuagi@gmail.com>
2024-01-19 10:04:22 +0000

    subredit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,13 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    try:
-        response = requests.get(url, allow_redirects=False)
-        if response.status_code == 200:
-            reddit_data = response.json()['data']
-            subscribers_count = reddit_data['subreddit_subscribers']
-            return subscribers_count
-        else:
-            return 0
-    except Exception as e:
-        print(f"error: {str(e)}")
+    response = requests.get(url, allow_redirects=False)
+
+    if response.status_code == 404:
         return 0
+    # converts response into python dict 
+    # Extracts data from json response
+    reddit_data = response.json().get("data")
+
+    #returns subscribers
+    return reddit_data.get("subscribers")
