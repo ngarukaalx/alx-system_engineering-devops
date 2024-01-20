7c245f7081602e594de73ff76ae97982feef629e
ngarukaalx <hiramkabuagi@gmail.com>
2024-01-20 12:37:23 +0000

    fixs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False)
 
     if response.status_code == 404:
